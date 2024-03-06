0f27c27f05bd8ae3c960e2d976a98e9b3b422a08
Gustavo Carvalho <gustavocarvalho.gc@gmail.com>
2024-03-06 12:42:34 -0300

    aula 01 e 02

---
+++ dev_web2/aula01/src/exercicio01.ts
@@ -0,0 +1,8 @@
+// function formatar(entrada: string): string {
+//   return `<div>${entrada}</div>`;
+// }
+
+const f = (entrada: string): string => `<div>${entrada}</div>`;
+
+console.log(f("Ana"));
+console.log(f("12"));
